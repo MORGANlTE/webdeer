d0f144ff0ae287bb0b5b0a4283b9a5358ce3d7e6
Laurens Arnauts <laurens.arnauts@student.hogent.be>
2023-04-27 22:58:48 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { DefaultRoute, Routes, Route, BrowserRouter as Router, Navigate } from "react-router-dom";
+import About from './pages/About';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route 
+            path="/about"
+            element={<About/>}
+            />
+          <Route path="/home" element={<Navigate to="/" replace={true} />} />
+          <Route exact path="/" element={<Home/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
