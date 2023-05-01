c8fd8155cccb87ad3b23a75bcbcb8439dc99a22f
Morganite <67431522+MORGANlTE@users.noreply.github.com>
2023-05-01 13:09:23 +0200

    seperation

---
+++ src/components/Global/StappenPlan.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import bronze from '../../images/ranks/bronze.png'
+import silver from '../../images/ranks/silver.png'
+import gold from '../../images/ranks/gold.png'
+import diamond from '../../images/ranks/diamond.png'
+import webdeer from "../../images/deer.png"; // with import
+import TextPart from '../../components/Quiz/TextPart'
+import MultipleChoice4 from '../../components/Quiz/MultipleChoice4'
+import AnswerWithImage from '../../components/Quiz/AnswerWithImage'
+import { useQuiz } from '../../context/QuizContext'
+
+const StappenPlan = () => {
+  const { stap, categories, currentQuestion, setNiveau, niveau, nextStap } = useQuiz();
+  const updateNiveau = (nr) => {
+    console.log('test')
+    setNiveau(nr)
+    nextStap();
+  };
+  return (
+    <>
+        {
+            stap===0 && (
+            <TextPart text="Welkom gebruiker" subtext="Om te starten zouden we je graag je internet vaardigheidsniveau inschatten" imgurl={webdeer}/>
+            )
+        }
+        {
+          stap===1 && 
+          <>
+            <MultipleChoice4 titel="Hoe schat je je eigen vaardigheden in?">
+              <AnswerWithImage onClick={() => updateNiveau(1)} text="Geen internet vaardigheden" url={bronze}/>
+              <AnswerWithImage onClick={() => updateNiveau(2)} text="Beperkte internet vaardigheid" url={silver}/>
+              <AnswerWithImage onClick={() => updateNiveau(3)} text="Goed internetvaardig" url={gold}/>
+              <AnswerWithImage onClick={() => updateNiveau(4)} text="Ik heb alle basisvaardigheden van het internet" url={diamond}/>
+            </MultipleChoice4>
+          </>
+        }
+    </>
+  )
+}
+
+export default StappenPlan
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,17 +1,10 @@
 import React, { useCallback, useContext, useState } from 'react'
 import Navbar from '../components/Global/Navbar'
-import MultipleChoice4 from '../components/Quiz/MultipleChoice4'
-import AnswerWithImage from '../components/Quiz/AnswerWithImage'
-import OneImageQuestion from '../components/Quiz/OneImageQuestion'
-import AnswerNoImage from '../components/Quiz/AnswerNoImage'
 import { useQuiz } from '../context/QuizContext'
-import webdeer from "../images/deer.png"; // with import
-import TextPart from '../components/Quiz/TextPart'
+
 import ProgressBar from '../components/Global/ProgressBar'
-import bronze from '../images/ranks/bronze.png'
-import silver from '../images/ranks/silver.png'
-import gold from '../images/ranks/gold.png'
-import diamond from '../images/ranks/diamond.png'
+import StappenPlan from '../components/Global/StappenPlan'
+
 
 const Quiz = () => {
 	const { stap, categories, currentQuestion, setNiveau, niveau, nextStap } = useQuiz();
@@ -23,32 +16,11 @@ const Quiz = () => {
   return (
     <>
         <Navbar/>
-        {
-          stap===0 && (
-            <TextPart text="Welkom gebruiker" subtext="Om te starten zouden we je graag je internet vaardigheidsniveau inschatten" imgurl={webdeer}/>
-          )
-        }
-        {/* <div>
-          <h2>{questionData.question}</h2>
-          <ul>
-            {questionData.choices.map((choice, index) => (
-              <li key={index} onClick={() => handleChoiceClick(choice)}>
-                {choice}
-              </li>
-            ))}
-          </ul>
-        </div> */}
-        {
-          stap===1 && 
-          <>
-            <MultipleChoice4 titel="Hoe schat je je eigen vaardigheden in?">
-              <AnswerWithImage onClick={() => updateNiveau(1)} text="Geen internet vaardigheden" url={bronze}/>
-              <AnswerWithImage onClick={() => updateNiveau(2)} text="Beperkte internet vaardigheid" url={silver}/>
-              <AnswerWithImage onClick={() => updateNiveau(3)} text="Goed internetvaardig" url={gold}/>
-              <AnswerWithImage onClick={() => updateNiveau(4)} text="Ik heb alle basisvaardigheden van het internet" url={diamond}/>
-            </MultipleChoice4>
-          </>
-        }
+        
+
+        
+       <StappenPlan /> 
+        
       <ProgressBar completed={(currentQuestion/categories.length)*100}/>
         <p>{stap}</p>
         <p>{niveau}</p>
