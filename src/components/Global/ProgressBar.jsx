ac2165ebfb6d7967e8ae59d1248467fe4c0ca18a
Laurens Arnauts <laurens.arnauts@student.hogent.be>
2023-05-01 22:44:35 +0200

    fix

--- src/components/Global/ProgressBar.jsx
+++ src/components/Global/ProgressBar.jsx
@@ -1,32 +1,40 @@
 import React from "react";
 
 const ProgressBar = (props) => {
-    const { completed } = props;
-  
-    const containerStyles = {
-      height: 20,
-      backgroundColor: "#e0e0de",
-    }
-  
-    const fillerStyles = {
-      height: '100%',
-      width: `${completed}%`,
-      borderRadius: 'inherit',
-      transition: 'width 2s ease-in-out',
-      textAlign: 'right'
-    }
-  
-    const labelStyles = {
-      padding: 5,
-      color: 'white',
-      fontWeight: 'bold',
-    }
-  
-    return (
-      <div style={containerStyles} className="mx-52 rounded-sm my-5">
-        <div style={fillerStyles} className="bg-blue-900 font-fira text-white animate-loading"/>
-      </div>
-    );
+  const { completed } = props;
+
+  const containerStyles = {
+    height: 20,
+    backgroundColor: "#e0e0de",
   };
-  
-  export default ProgressBar;
+
+  const fillerStyles = {
+    height: "100%",
+    width: `${completed}%`,
+    borderRadius: "inherit",
+    transition: "width 2s ease-in-out",
+    textAlign: "right",
+  };
+
+  const labelStyles = {
+    padding: 5,
+    color: "white",
+    fontWeight: "bold",
+  };
+
+  return (
+    <div className="fixed bottom-0 flex flex-col items-center w-4/5 h-12 -translate-x-1/2 rounded-t-full align left-1/2 bg-background drop-shadow-[0_0_5px_rgba(68,150,255,0.10)] ">
+      <div
+        style={containerStyles}
+        className="fixed w-4/5 h-full my-5 rounded-sm"
+      >
+        <div
+          style={fillerStyles}
+          className="text-white bg-blue-900 font-fira animate-loading"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
